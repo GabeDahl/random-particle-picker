dbee7dcfea918dabc3083618e5852205cca0f92b
Gabriel <gabrdahl@gmail.com>
2023-01-27 01:04:36 -0600

    gee i hope this fixes it

--- src/components/dom/controls/global/ParticleSpeedControls.tsx
+++ src/components/dom/controls/global/ParticleSpeedControls.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Slider, Stack } from '@mui/material'
 import { store, useAppDispatch } from '@/redux/store'
 import Typography from '@mui/material/Typography'
 import { updateSpeed } from '@/redux/features/controlsSlice'
+import { useIsLargeViewport } from '@/hooks/useIsLargeViewport'
 
 export const ParticleSpeedControls = () => {
+  const isLargeViewport = useIsLargeViewport()
   const dispatch = useAppDispatch()
   const [inputValue, setInputValue] = useState(() => {
     return store.getState().controls.speed
@@ -17,6 +19,10 @@ export const ParticleSpeedControls = () => {
     dispatch(updateSpeed(newValue))
   }
 
+  useEffect(() => {
+    if (isLargeViewport) return
+    dispatch(updateSpeed(8))
+  }, [])
   return (
     <Stack direction={'column'} spacing={1} p={1}>
       <Typography sx={{ color: '#d1d1d1' }}>Particle Speed</Typography>
